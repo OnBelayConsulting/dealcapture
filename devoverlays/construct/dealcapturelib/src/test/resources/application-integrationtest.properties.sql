2846cff37de95526475af41805958c4537b442ce
Michael LeF <lefeuvrem@gmail.com>
2024-02-17 15:54:40 -0800

    fix: Fixed issue with git actions database

---
+++ devoverlays/construct/dealcapturelib/src/test/resources/application-integrationtest.properties.sql
@@ -0,0 +1,8 @@
+spring.datasource.driverClassName=com.microsoft.sqlserver.jdbc.SQLServerDriver
+spring.datasource.url=jdbc:sqlserver://localhost:1433;databaseName=obdealcapture_test;trustServerCertificate=true
+spring.datasource.username=dealcapture
+spring.datasource.password=dealcapture
+spring.profiles.active=test
+spring.main.allow-bean-definition-overriding=true
+//spring.jpa.hibernate.use-new-id-generator-mappings=true
+spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.SQLServer2012Dialect
---
+++ devoverlays/database/jdbc.properties.sql
@@ -0,0 +1,14 @@
+# Properties file with JDBC-related settings.
+spring.datasource.driverClassName=com.microsoft.sqlserver.jdbc.SQLServerDriver
+
+# Change the jdbc.url property to the database URL where your schema exists!
+jdbc.url=jdbc:sqlserver://localhost:1433;databaseName=obdealcapture;trustServerCertificate=true
+
+# Change the username, password and connectString properties to your specific schema details:
+# Select one of the schemas from the VM Environment.
+jdbc.username=dealcapture
+jdbc.password=dealcapture
+
+# Property that determines the Hibernate dialect
+# (only applied with "applicationContext-hibernate.xml")
+hibernate.dialect=org.hibernate.dialect.SQLServer2012Dialect
